464b0519da1134f6a8654d4e836301d632b4207b
unknown <lencompsci@gmail.com>
2022-01-17 10:53:35 -0330

    just some changes in testStuff junk file

--- lane_mate_frontend/src/testStuff.js
+++ lane_mate_frontend/src/testStuff.js
@@ -450,3 +450,9 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
+
+  function addDashes(f) {
+    f_val = f.value.replace(/\D[^\.]/g, "");
+    f.value =
+      f_val.slice(0, 3) + "-" + f_val.slice(3, 6) + "-" + f_val.slice(6);
+  }
